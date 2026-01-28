4fc545b0a16d6a2d3c249bd6e7c9958ce41d9a6a
SpearZero <spearzero0@gmail.com>
2026-01-29 08:10:22 +0900

    refactor: AppAplication 코틀린으로 변경

--- src/main/kotlin/com/rtc/app/AppApplication.kt
+++ src/main/kotlin/com/rtc/app/AppApplication.kt
@@ -2,16 +2,13 @@ package com.rtc.app
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @EnableJpaAuditing
 @SpringBootApplication
-class AppApplication {
+class AppApplication
 
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            SpringApplication.run(AppApplication::class.java, *args)
-        }
-    }
+fun main(args: Array<String>) {
+    runApplication<AppApplication>(*args)
 }
